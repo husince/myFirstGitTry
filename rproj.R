9587c5c30ad67a6afdba8ee50a4f421592d9c88b
yingwei <husince@gmail.com>
2018-05-02 17:26:55 -0400

    b1

--- rproj.R
+++ rproj.R
@@ -2,5 +2,5 @@ head(iris)
 
 plot(x=iris$Sepal.Length,y=iris$Sepal.Width,xlab = 'sepal length', ylab = 'sepal width')
 
-
-hist(x=iris$Sepal.Length,xlab='sepal length')
+cor(iris[,-5])
+pairs(iris[,-5])
